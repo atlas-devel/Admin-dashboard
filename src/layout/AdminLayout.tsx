cc540b802a2db5eb8f02f413dfc755e7804dbf73
Leon <irakaramale@gmail.com>
2026-04-13 03:22:55 +0200

    feat:create usermanagement table

--- src/layout/AdminLayout.tsx
+++ src/layout/AdminLayout.tsx
@@ -6,11 +6,11 @@ import Sidebar from "../components/admin/common/Sidebar/Sidebar";
 function AdminLayout() {
   return (
     <SidebarProvider>
-      <section className="h-screen max-w-[2000px] mx-auto flex overflow-hidden  dark:bg-background bg-blue-50/40">
+      <section className="h-screen max-w-[2000px] mx-auto flex overflow-x-hidden overflow-y-hidden dark:bg-background bg-blue-50/40">
         <Sidebar />
-        <div className="flex-1 ">
+        <div className="flex-1 min-w-0 ">
           <Navigation />
-          <main className="relative h-[calc(100vh-80px)]  overflow-y-auto  mx-auto p-4 md:p-6 lg:p-8  ">
+          <main className="relative h-[calc(100vh-80px)] overflow-y-auto mx-auto p-4 md:p-6 lg:p-8">
             <Outlet />
           </main>
         </div>
